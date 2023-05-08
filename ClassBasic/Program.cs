063d08f353532dc09762aeb903718cf74e49c947
Juwan Lee <ljw05w5@gmail.com>
2023-05-08 12:12:53 +0900

    초기 커밋.

---
+++ ClassBasic/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace NewClass // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
